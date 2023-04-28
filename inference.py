80f6e7017e55983f07b327a72e80d083bcb52b77
Walchshofer <p.walchshofer@gmail.com>
2023-04-28 19:21:04 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 from os import listdir, path
 import numpy as np
-import scipy, cv2, os, sys, argparse, audio
+import scipy, cv2, os, sys, argparse, local_projects.repos.Wav2Lip.audio as audio
 import json, subprocess, random, string
 from tqdm import tqdm
 from glob import glob
